7a7b6858a9b95dfac02c53e820d79a6453e64016
Elliot (aka. Proliecan) <65495509+Proliecan@users.noreply.github.com>
2023-03-06 13:38:10 +0100

    Tile Brush

---
+++ Assets/Code/Components/InputController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Code.components;
+using UnityEngine;
+
+namespace Code.Components
+{
+    public class InputController : MonoBehaviour
+    {
+        private Camera _mainCam;
+        private bool _mainCamNotNull;
+
+#if UNITY_EDITOR
+        public bool debugMode = false;
+#endif
+
+        private void Start(){
+            _mainCam = Camera.main;
+            _mainCamNotNull = _mainCam != null;
+        }
+
+        private void Update(){
+            // get mouse position
+            if (_mainCamNotNull){
+                Vector2 mousePosition = _mainCam.ScreenToWorldPoint(Input.mousePosition);
+                if (Input.GetMouseButton(0)){
+                    TileBrush.Instance.Paint(mousePosition);
+
+#if UNITY_EDITOR
+                    if (debugMode) Debug.Log($"Painted tile at {mousePosition}");
+#endif
+                }
+
+                if (Input.GetMouseButton(1)){
+                    TileBrush.Instance.Erase(mousePosition);
+
+#if UNITY_EDITOR
+                    if (debugMode) Debug.Log($"Erased tile at {mousePosition}");
+#endif
+                }
+            }
+        }
+    }
+}
